7ae95da17a2481a47dc68e6e597a2fd633bec61f
DylanSantiago <2dylansantiago@gmail.com>
2022-10-23 01:43:44 -0400

    testing testing 123

--- Develop/README.md
+++ Develop/README.md
@@ -1,7 +1,7 @@
-# Professional ReadMe Generator 
-  Boost Software
- ## Licensed by Boost Software
+#Professional ReadMe Generator
+  ## Licensed by Boost Software
   <br/>[![License: Boost Software](https://img.shields.io/badge/License-Boost-green.svg)](https://opensource.org/licenses/BSL-1.0)
+
 # Table of Contents:
 * [Description](#description)
 * [Installation](#installation)
@@ -13,25 +13,25 @@
 * [Questions](#questions)
 
 ## Description
-A tool used for Software Developers to create a professionally strcutured ReadMe for their projects
+A tool made for Software Developers to create a well structured and professional ReadMe.
 
 ## Installation
-cloning this repository onto your machine
+Cloning the repository and then installing inquirer through npm.
 
 ## Usage
-after cloning, you must enter the develop folder through your command line and then run node index.js and then proceed to answer each question.
+After cloning the repository, you must install inquirer through your command line and answer all necessary prompts.
 
 ## License
 Boost Software
 
 ## Reference
-Utilizing the course material
+Reviewing the class material helped me complete this assignment.
 
 ## Tests
 npm test
 
 ## Contribution
-useres cannot contribute however they can suggest ideas by reaching out to me 
+No, however you can contact me with suggestions to improve this project. All feedback is welcomed. 
 
 ## Questions
 Please feel free to follow me on Github: DylanSantiago and if you have any questions or would like to collaborate on a project Email me at 2dylansantiago@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,78 +1,155 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  switch(license) {
-   case "Apache 2.0":
-   license ='<br/>[![License: Apache 2.0](https://img.shields.io/badge/License-Apache-blue.svg)]'
-   return license
-   break;
-   case "MIT":
-   license ='<br/>[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg)]'
-   return license
-   break;
-   case "Boost Software":
-   license ='<br/>[![License: Boost Software](https://img.shields.io/badge/License-Boost-green.svg)]'
-   return license
-   break;
-   case "none":
-   license =" ";
-   return license
-   break;
-  }
-};
+//function renderLicenseBadge(license) {
+//  switch(license) {
+//   case "Apache 2.0":
+//   license ='<br/>[![License: Apache 2.0](https://img.shields.io/badge/License-Apache-blue.svg)]'
+//   return license
+//   break;
+//   case "MIT":
+//   license ='<br/>[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg)]'
+//   return license
+//   break;
+//   case "Boost Software":
+//   license ='<br/>[![License: Boost Software](https://img.shields.io/badge/License-Boost-green.svg)]'
+//   return license
+//   break;
+//   case "none":
+//   license =" ";
+//   return license
+//   break;
+//  }
+//};
+//function renderLicenseBadge() {
+//  let licenseBadge = "";
+//  let licenseLink = "";
+//  let licenseSection = "";
+
+//  if(data.license === "MIT") {
+//    licenseBadge = '<br/>[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg)]';
+//    licenseLink = '(https://opensource.org/licenses/MIT)';
+//    licenseSection = '## Licensed by MIT;';
+//  } else if (data.license === "Apache 2.0") {
+//    licenseBadge = '<br/>[![License: Apache 2.0](https://img.shields.io/badge/License-Apache-blue.svg)]'
+//    licenseLink = '(https://opensource.org/licenses/Apache-2.0)';
+//    licenseSection = 'Licensed by Apache 2.0';
+//  } else if (data.license === "Boost Software") {
+//    licenseBadge = '<br/>[![License: Boost Software](https://img.shields.io/badge/License-Boost-green.svg)]'
+//    licenseLink = '(https://opensource.org/licenses/BSL-1.0)';
+//    licenseSection = '## Licensed by Boost Software';
+//  } else if (data.license === "No license") {
+//    licenseBadge = "";
+//    licenseLink = '';
+//    licenseSection = '';
+//  };
+// };
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  switch(license) {
-   case "Apache 2.0":
-    license ='(https://opensource.org/licenses/Apache-2.0)'
-    return license
-    break;
-    case "MIT":
-    license ='(https://opensource.org/licenses/MIT)'
-    return license
-    break;
-    case "Boost Software":
-    license ='(https://opensource.org/licenses/BSL-1.0)'
-    return license
-    break;
-    case "none":
-    license =" ";
-    return license
-    break;
-  }
-};
+//function renderLicenseLink(license) {
+//  switch(license) {
+//   case "Apache 2.0":
+//    license ='(https://opensource.org/licenses/Apache-2.0)'
+//    return license
+//    break;
+//    case "MIT":
+//    license ='(https://opensource.org/licenses/MIT)'
+//    return license
+//    break;
+//    case "Boost Software":
+//    license ='(https://opensource.org/licenses/BSL-1.0)'
+//    return license
+//    break;
+//    case "none":
+//    license =" ";
+//    return license
+//    break;
+//  }
+// };
+
+//function renderLicenseLink(license) {
+
+//  if(data.license === "Apache 2.0") {
+//    license = '(https://opensource.org/licenses/Apache-2.0)'
+//    return license
+//  } else if (data.license === "MIT") {
+//    license = '(https://opensource.org/licenses/MIT)'
+//    return license
+//  } else if (data.license === "Boost Software") {
+//    license = '(https://opensource.org/licenses/BSL-1.0)'
+//    return license
+//  } else if (data.license === "No license") {
+//    license = ""
+//    return license
+//  };
+//}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  switch(license) {
-    case "Apache 2.0":
-    license ="# Apache 2.0\n ## Licensed by Apache 2.0";
-    return license
-    break;
-    case "MIT":
-    license ="MIT\n ## Licensed by MIT";
-    return license
-    break;
-    case "Boost Software":
-    license ="Boost Software\n ## Licensed by Boost Software";
-    return license
-    break;
-    case "none":
-    license =" ";
-    return license
-    break;
-  }
+//function renderLicenseSection(license) {
+//  switch(license) {
+//    case "Apache 2.0":
+//    license ="# Apache 2.0\n ## Licensed by Apache 2.0";
+//    return license
+//    break;
+//    case "MIT":
+//    license ="MIT\n ## Licensed by MIT";
+//    return license
+//    break;
+//    case "Boost Software":
+//    license ="Boost Software\n ## Licensed by Boost Software";
+//    return license
+//    break;
+//    case "none":
+//    license =" ";
+//    return license
+//    break;
+//  }
  
-};
-
+//};
+//function renderLicenseSection(license) {
+//  if(data.license === "Apache 2.0") {
+//    license = '## Licensed by Apache 2.0';
+//    return license
+//  } else if (data.license === "MIT") {
+//    license = '## Licensed by MIT;'
+//    return license
+//  } else if (data.license === "Boost Software") {
+//    license = '## Licensed by Boost Software';
+//    return license
+//  } else if (data.license === "No license") {
+//    license = ""
+//    return license
+//  };
+//}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} 
-  ${renderLicenseSection(data.license)}
-  ${renderLicenseBadge(data.license)}${renderLicenseLink(data.license)}
+  let licenseBadge = "";
+  let licenseLink = "";
+  let licenseSection = "";
+
+  if(data.license === "MIT") {
+    licenseBadge = '<br/>[![License: MIT](https://img.shields.io/badge/License-MIT-red.svg)]';
+    licenseLink = '(https://opensource.org/licenses/MIT)';
+    licenseSection = '## Licensed by MIT;';
+  } else if (data.license === "Apache 2.0") {
+    licenseBadge = '<br/>[![License: Apache 2.0](https://img.shields.io/badge/License-Apache-blue.svg)]'
+    licenseLink = '(https://opensource.org/licenses/Apache-2.0)';
+    licenseSection = 'Licensed by Apache 2.0';
+  } else if (data.license === "Boost Software") {
+    licenseBadge = '<br/>[![License: Boost Software](https://img.shields.io/badge/License-Boost-green.svg)]'
+    licenseLink = '(https://opensource.org/licenses/BSL-1.0)';
+    licenseSection = '## Licensed by Boost Software';
+  } else if (data.license === "No license") {
+    licenseBadge = "";
+    licenseLink = '';
+    licenseSection = '';
+  };
+
+  return `#${data.title}
+  ${licenseSection}
+  ${licenseBadge}${licenseLink}
+
 # Table of Contents:
 * [Description](#description)
 * [Installation](#installation)
